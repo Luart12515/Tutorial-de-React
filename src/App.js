b2a3ab89429f36ba0f9aec65921fab97bfdbf48d
Raul <jrgonzalez@occ.com.mx>
2018-10-31 13:06:45 -0600

    Segundo dia

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/navigation'
+import {superdatos} from './datos.json'
 
 class App extends Component {
+  constructor(){
+    super(); //Es necesario " super" para poder heredar la funcionalidad de React 
+    this.state = { //Estado de los datos en una aplicacion de React
+      /* este es un ejemplo de como funciona la  
+      titulo: 'Aplicacion de Trabajos', //son los datos del objeto a transmitir 
+      NumeroTarea: 10   
+      */
+    } 
+
+  }
   render() {
     return (
       <div className="App">
-        <Navigation/>
+        <Navigation titulo="Seccion de pruebas"/>
+        <Navigation titulo="Mejorando cada secccion"/> {/* El titulo lo ingreamos desde navigations */}
+        
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Tutorial de React fazt</h1>
-      
+        
+    {/* this.state.titulo  Estrae la informacion de campo state. Titulo*/}  numero de tareas son {/* this.state.numeroTarea */ }
+        {3+2}
       </div>
     );
   }
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,8 @@ class Navigation extends Component{
     render() {
         return (                   
             <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-                <a className="navbar-brand" href="#">Logo</a>
+                
+                <a className="navbar-brand" href="#">{ this.props.titulo} {/* "props" es un objeto el cual lo estamos enviando  a traves del "this." para que pueda ser usado mas adelante  */ }</a>
                 <ul className="navbar-nav">
                     <li className="nav-item">
                         <a className="nav-link" href="#">Link 1</a>
@@ -15,6 +16,7 @@ class Navigation extends Component{
                     <li className="nav-item">
                         <a className="nav-link" href="#">Link 3</a>
                     </li>
+               
                 </ul>
             </nav>
         )
